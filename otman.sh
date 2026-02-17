c575ac362e7aa77c35719303948e956ddb13d9d9
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-02-17 19:15:02 +0700

    Create otman.sh

---
+++ otman.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#--[1] dialog
+function otman_dialog() {
+options_folder=$1
+otman_folder
+}
+#--[2]folder
+#-- [2][1] create folder
+function otman_folder_create() {
+  
+  if [[ $options_folder != "create" ]]; then
+    return
+    fi
+  read -p "masukan nama folder : " folder_name &&
+  mkdir $folder_name
+}
+
+
+function otman_folder() {
+otman_folder_create
+}
+
+
+function otman() {
+otman_dialog $1
+}
+otman $1
