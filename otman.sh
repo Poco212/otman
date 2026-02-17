ff6e6e32bfa49a936295b4cfce06608ff12de6da
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-02-17 20:04:22 +0700

    update

--- otman.sh
+++ otman.sh
@@ -23,13 +23,23 @@ function otman_folder_create() {
     return
     fi
   read -p "masukan nama folder : " folder_name &&
-  mkdir $folder_name
+  mkdir -p $folder_name
 }
 
 #-- [2][3] delete folder
 function otman_folder_delete() {
   
   if [[ $options_folder != "delete" ]]; then
+    return
+    fi
+  read -p "masukan nama folder yang ingin dihapus : " folder_delete_name &&
+  rm -fr $folder_delete_name
+}
+
+#-- [2][4] rename folder
+function otman_folder_rename() {
+  
+  if [[ $options_folder != "rename" ]]; then
     return
     fi
   read -p "masukan nama folder yang ingin dihapus : " folder_delete_name &&
@@ -40,6 +50,7 @@ function otman_folder() {
 otman_folder_list
 otman_folder_create
 otman_folder_delete
+otman_folder_rename
 }
 
 
