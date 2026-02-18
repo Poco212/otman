69f520c75ece7be863ca2b370facff6c90d94134
Achmad Baihaqi <138300409+Poco212@users.noreply.github.com>
2026-02-18 23:49:29 +0700

    update

--- otman.sh
+++ otman.sh
@@ -9,6 +9,7 @@ touch black/dot.txt black/dit.txt &&
 #delete folder dan file
 rm -fr white black/dit.txt &&
 #isi file line 1 hello dan line 2 world
+printf "hello \nworld" > black/dot.txt
 #edit file world tambahin !
 # jadikan hello dan world menjadi 1 line
 # copy file
